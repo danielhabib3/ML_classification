c8e0206634cede7705ecb7fd95993dc0d5c72b38
danielhabib3 <danielhabib301@gmail.com>
2025-04-04 10:18:03 +0200

    regression

---
+++ ML_regression/regression_train.py
@@ -0,0 +1,64 @@
+import csv
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.preprocessing import LabelEncoder
+
+path_classification_data = "data/regression/"
+path_train_csv = f"{path_classification_data}/train.csv"
+path_test_csv = f"{path_classification_data}/test.csv"
+
+with open(path_train_csv) as my_csv_file:
+  header_and_data = list(csv.reader(my_csv_file, delimiter=','))
+
+# Conversion en DataFrame
+df = pd.DataFrame(header_and_data[1:], columns=header_and_data[0])
+
+column_to_delete = ['id']
+
+column_numeric = ['hc', 'max_power', 'weight_min', 'weight_max', 'urb_cons', 'exturb_cons', 'overall_cons', 'co', 'nox', 'hcnox', 'ptcl', 'co2']
+
+
+# Supprimer les lignes où 'nox' est une chaîne vide ''
+# Quand y a pas nox y a pas hc et hcnox
+df = df[df['nox'] != '']
+df = df[df['ptcl'] != '']
+df = df[df['exturb_cons'] != '']
+df = df[df['urb_cons'] != '']
+
+# Remplacer les valeurs manquantes de 'hcnox' par la somme de 'hc' et 'nox'
+df['hcnox'] = df.apply(
+  lambda row: float(row['hc']) + float(row['nox']) if row['hcnox'] == '' else float(row['hcnox']),
+  axis=1
+)
+
+# Remplacer les valeurs manquantes de 'hc' par la différence entre 'hcnox' et 'nox'
+df['hc'] = df.apply(
+  lambda row: float(row['hcnox']) - float(row['nox']) if row['hc'] == '' else float(row['hc']),
+  axis=1
+)
+
+# Identifier les colonnes non numériques
+non_numeric_columns = [col for col in df.columns if col not in column_numeric and col not in column_to_delete]
+
+# Appliquer LabelEncoder sur les colonnes non numériques
+label_encoders = {}
+for col in non_numeric_columns:
+  le = LabelEncoder()
+  df[col] = le.fit_transform(df[col])
+  label_encoders[col] = le
+
+
+
+# Séparer les 90% premiers données pour l'entraînement
+train_size = int(len(df) * 0.7)
+df_train = df.iloc[:train_size].copy()
+
+df_test = df.iloc[train_size:].copy()
+
+
+
+
+
+# Nettoyage des données inutiles car on voit déjà sur Kaggle qu'il n'y a pas de données manquantes, valeurs abberantes ...
+
+print(df)
